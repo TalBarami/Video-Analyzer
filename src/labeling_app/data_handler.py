57ed5fb0554e1397432fe9d9138de315bd458b7d
Tal Baramit <baramit@post.bgu.ac.il>
2021-12-19 15:05:06 +0200

    Added legs movement class.

--- src/labeling_app/data_handler.py
+++ src/labeling_app/data_handler.py
@@ -15,7 +15,7 @@ class DataHandler:
         self.df = None
         self.idx = 0
         self.movements = ['Hand flapping', 'Tapping', 'Fingers', 'Clapping', 'Body rocking', 'Toe walking', 'Spinning in circle',
-                          'Back and forth', 'Head movement', 'Tremor', 'Playing with object', 'Jumping in place']
+                          'Back and forth', 'Head movement', 'Tremor', 'Playing with object', 'Jumping in place', 'Legs Movement']
         self.movements.sort()
         self.movements.append('Other')
         # self.colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Cyan', 'Gray', 'Brown']
